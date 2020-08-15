5f184e933faeb0397b83228940ccac707384833a
ghozaa <ghoza16m@gmail.com>
2020-08-15 22:15:32 +0700

    first#1

---
+++ app/src/main/java/com/example/simplenotesapp/db/NoteHelper.kt
@@ -0,0 +1,108 @@
+package com.example.simplenotesapp.db
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import com.example.simplenotesapp.db.DatabaseContract.NoteColumns.Companion.TABLE_NAME
+import com.example.simplenotesapp.db.DatabaseContract.NoteColumns.Companion._ID
+import java.sql.SQLException
+
+class NoteHelper(context: Context) {
+
+    companion object {
+        private const val DATABASE_TABLE = TABLE_NAME
+        private lateinit var dataBaseHelper: DatabaseHelper
+
+        private var INSTANCE: NoteHelper? = null
+        fun getInstance(context: Context): NoteHelper =
+            INSTANCE ?: synchronized(this) {
+                INSTANCE ?: NoteHelper(context)
+            }
+
+        private lateinit var database: SQLiteDatabase
+
+    }
+
+    init {
+        dataBaseHelper = DatabaseHelper(context)
+    }
+
+    @Throws(SQLException::class)
+    fun open() {
+        database = dataBaseHelper.writableDatabase
+    }
+
+    fun close() {
+        dataBaseHelper.close()
+
+        if (database.isOpen)
+            database.close()
+    }
+
+    /**
+     * Ambil data dari semua note yang ada di dalam database
+     *
+     * @return cursor hasil queryAll
+     */
+    fun queryAll(): Cursor {
+        return database.query(
+            DATABASE_TABLE,
+            null,
+            null,
+            null,
+            null,
+            null,
+            "$_ID ASC"
+        )
+    }
+
+    /**
+     * Ambil data dari note berdasarakan parameter id
+     *
+     * @param id id note yang dicari
+     * @return cursor hasil queryAll
+     */
+    fun queryById(id: String): Cursor {
+        return database.query(
+            DATABASE_TABLE,
+            null,
+            "$_ID = ?",
+            arrayOf(id),
+            null,
+            null,
+            null,
+            null)
+    }
+
+    /**
+     * Simpan data ke dalam database
+     *
+     * @param values nilai data yang akan di simpan
+     * @return long id dari data yang baru saja di masukkan
+     */
+    fun insert(values: ContentValues?): Long {
+        return database.insert(DATABASE_TABLE, null, values)
+    }
+
+    /**
+     * Update data dalam database
+     *
+     * @param id     data dengan id berapa yang akan di update
+     * @param values nilai data baru
+     * @return int jumlah data yang ter-update
+     */
+    fun update(id: String, values: ContentValues?): Int {
+        return database.update(DATABASE_TABLE, values, "$_ID = ?", arrayOf(id))
+    }
+
+    /**
+     * Delete data dalam database
+     *
+     * @param id data dengan id berapa yang akan di delete
+     * @return int jumlah data yang ter-delete
+     */
+    fun deleteById(id: String): Int {
+        return database.delete(DATABASE_TABLE, "$_ID = '$id'", null)
+    }
+}
